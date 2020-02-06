bafc8bace453fd0a845a1a0ea0b5e67b5371277a
mariuslhl <mariuslhl@gmail.com>
2020-02-06 21:33:12 +0100

    gogo

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ PrivateMessage.destroy_all
 end
 
 10.times do
-  User.create(first_name:Faker::Name.first_name  , last_name: Faker::Name.last_name , description: Faker::Quote.famous_last_words , email: Faker::Internet.email, age: 31, city_id: Faker::Number.within(range: City.first.id..City.last.id))
+  User.create(first_name:Faker::Name.first_name  , last_name: Faker::Name.last_name , description: Faker::Quote.famous_last_words , email: Faker::Internet.email, age: rand(18..78), city_id: Faker::Number.within(range: City.first.id..City.last.id))
 end
 
 20.times do
